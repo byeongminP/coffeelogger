043a594fdd1c38c9c74f948ac9e156afad0d1752
Byeong Min Park <byeongmin@berkeley.edu>
2020-12-19 14:33:03 -0800

    hbs: working on lists

--- database.sql
+++ database.sql
@@ -8,9 +8,20 @@ CREATE TABLE users (
 
 CREATE TABLE coffeelog (
   log_id SERIAL PRIMARY KEY,
-  log_num VARCHAR(31),
   author VARCHAR(2047),
-  description VARCHAR(255),
+  coffee_name VARCHAR(63),
+	coffee_roaster VARCHAR(63),
+	coffee_date VARCHAR(63),
+  brew_date VARCHAR(63),
+	brew_method VARCHAR(63),
+	brew_grind VARCHAR(63),
+	brew_dose VARCHAR(63),
+	brew_yield VARCHAR(63),
+	brew_temp VARCHAR(63),
+	brew_time VARCHAR(63),
+	description VARCHAR(255),
+	taste VARCHAR(63),
+	comments VARCHAR(255),
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
--- routes/coffee.js
+++ routes/coffee.js
@@ -5,10 +5,40 @@ const pool = require('../config/db')
 
 router.post('/', ensureAuth, async (req, res) => {
 	try {
-    let numEntries = await pool.query('SELECT COUNT(*) FROM coffeelog WHERE author = $1', [req.user])
-		const logNum = parseInt(numEntries.rows[0].count) + 1
-		const { description } = req.body
-		await pool.query('INSERT INTO coffeelog (log_num, author, description) VALUES ($1, $2, $3)', [logNum, req.user, description])
+		const {
+			coffee_name,
+			coffee_roaster,
+			coffee_date,
+			brew_date,
+			brew_method,
+			brew_grind,
+			brew_dose,
+			brew_yield,
+			brew_temp,
+			brew_time,
+			description,
+			taste,
+			comments,
+		} = req.body
+		await pool.query(
+			'INSERT INTO coffeelog (author, coffee_name,  coffee_roaster, coffee_date, brew_date, brew_method, brew_grind, brew_dose, brew_yield, brew_temp, brew_time, description, taste, comments) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)',
+			[
+				req.user,
+				coffee_name,
+				coffee_roaster,
+				coffee_date,
+				brew_date,
+				brew_method,
+				brew_grind,
+				brew_dose,
+				brew_yield,
+				brew_temp,
+				brew_time,
+				description,
+				taste,
+				comments,
+			]
+		)
 		res.redirect('/entries')
 	} catch (err) {
 		console.error(err)
@@ -17,29 +47,29 @@ router.post('/', ensureAuth, async (req, res) => {
 })
 
 router.get('/', ensureAuth, async (req, res) => {
-  try {
+	try {
 		const entries = await pool.query('SELECT * FROM coffeelog WHERE author = $1', [req.user])
-    res.render('entries', {
-      title: 'Your Journal...',
-      user: req.user,
-      entries: entries.rows
-    })
-  } catch (err) {
-    console.error(err)
-    res.render('error/500')
-  }
+		res.render('entries', {
+			title: 'Your Journal...',
+			user: req.user,
+			entries: entries.rows,
+		})
+	} catch (err) {
+		console.error(err)
+		res.render('error/500')
+	}
 })
 
 router.delete('/:id', ensureAuth, async (req, res) => {
 	try {
-		let entry = await pool.query('SELECT * FROM coffeelog WHERE author = $1 AND log_num = $2 LIMIT 1', [
+		let entry = await pool.query('SELECT * FROM coffeelog WHERE author = $1 AND log_id = $2 LIMIT 1', [
 			req.user,
 			req.params.id,
 		])
 		if (!entry) {
 			return res.render('error/404')
 		}
-		await pool.query('DELETE FROM coffeelog WHERE author = $1 AND log_num = $2', [req.user, req.params.id])
+		await pool.query('DELETE FROM coffeelog WHERE author = $1 AND log_id = $2', [req.user, req.params.id])
 		res.redirect('/entries')
 	} catch (err) {
 		console.error(err)
@@ -48,14 +78,14 @@ router.delete('/:id', ensureAuth, async (req, res) => {
 })
 
 router.get('/create', ensureAuth, async (req, res) => {
-  try {
-    res.render('addentry', {
-      title: 'Add an Entry'
-    })
-  } catch (err) {
-    console.error(err)
-    res.render('error/500')
-  }
+	try {
+		res.render('addentry', {
+			title: 'Add an Entry',
+		})
+	} catch (err) {
+		console.error(err)
+		res.render('error/500')
+	}
 })
 
 module.exports = router
